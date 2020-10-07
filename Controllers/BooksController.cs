18b7b82cf5bf67881e7b213382c018a9a83f7136
Courtney Frey <66076696+speudusa@users.noreply.github.com>
2020-10-07 11:34:51 -0500

    Add project files.

---
+++ Controllers/BooksController.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+
+namespace BearPawPages.Controllers
+{
+    public class BooksController : Controller
+    {
+        static private List<string> Books = new List<string>();
+        
+        [HttpGet]
+        public IActionResult Index()
+        {
+            Books.Add("Pumpkin Soup");
+            Books.Add("Mr. Putter Paints the Porch");
+            Books.Add("Podkin One Ear");
+            ViewBag.books = Books;
+
+            return View();
+        }
+
+        [HttpGet]
+        public IActionResult Add()
+        {
+            return View();
+        }
+
+
+    }
+}
