1b274cb25ff038e1ee694d80cb7b871ba60ab61d
Courtney Frey <66076696+speudusa@users.noreply.github.com>
2020-10-07 17:10:12 -0500

    Build Book Model

--- Controllers/BooksController.cs
+++ Controllers/BooksController.cs
@@ -2,31 +2,40 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using BearPawPages.Models;
 using Microsoft.AspNetCore.Mvc;
 
 namespace BearPawPages.Controllers
 {
     public class BooksController : Controller
     {
-        static private List<string> Books = new List<string>();
+        static private List<Book> Books = new List<Book>();
         
         [HttpGet]
         public IActionResult Index()
         {
-            Books.Add("Pumpkin Soup");
-            Books.Add("Mr. Putter Paints the Porch");
-            Books.Add("Podkin One Ear");
             ViewBag.books = Books;
 
             return View();
         }
 
-        [HttpGet]
+        [HttpGet]  //this DISPLAYS the form
         public IActionResult Add()
         {
             return View();
         }
 
+        [HttpPost]  //Updates
+        [Route("/Books/Add")]  //This is WHERE we will update
+
+        public IActionResult NewBook(string title, string author, int totalPage, int currentPage, DateTime readingDate, string readingNotes)
+        {
+            Books.Add(new Book (title, author, totalPage, currentPage, readingDate, readingNotes));
+
+            return Redirect("/Books");
+        }
 
     }
 }
+
+//TODO:  Pick up at 14.3 Models & Data
---
+++ Models/Book.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace BearPawPages.Models
+{
+    public class Book
+    {
+        public string Title { get; set; }
+
+        public string Author { get; set; }
+
+        public int TotalPage { get; set; }
+
+        public int CurrentPage { get; set; }
+
+        public DateTime ReadingDate { get; set; }
+
+        public string ReadingNotes { get; set; }
+
+        public Book(string title, string author, int totalPage, int currentPage, DateTime readingDate, string readingNotes)
+        {
+            Title = title;
+            Author = author;
+            TotalPage = totalPage;
+            CurrentPage = currentPage;
+            ReadingDate = readingDate;
+            ReadingNotes = readingNotes;
+        }
+
+        public override string ToString()
+        {
+            return Title;
+        }
+    }
+}
